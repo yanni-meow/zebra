b465986b1f5e163f54f1f7b4bfae7da50c7dc788
yanni <yanni.mamporiya@gmail.com>
2020-09-24 21:44:41 +0300

    media > 375

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// const body = document.querySelector ('body')
+// const checkBox = document.querySelector ('.header__nav__check')
+// const menu = document.querySelector ('.header__nav__list')
+
+// if (body && checkBox) {
+//     body.addEventListener('click', function() {
+//     if(checkBox.checked){
+//         checkBox.checked = false;
+//         }
+//     })
+//     menu.addEventListener('click', function(e) {
+//         e.stopPropagation()
+//     })
+// }
+
+// $('header__nav').slideUp(2000).slideDown(2000);
+
+// if (body && checkBox) {
+//     if(checkBox.checked){
+//         menu.hide('3000')
+//     }
+// }
+
+// $('.header__nav__check').click(
+//     function a() {
+//         $('header__nav__list').show(3000)  
+//     }
+// )
+
+$()
