563331d53904a7ff6d53ca7ec4c5a563765e5ebd
yanni <yanni.mamporiya@gmail.com>
2020-09-28 00:41:50 +0300

    start clear js menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
-const body = document.querySelector('body');
-const clickPlace = document.querySelectorAll('.header__nav__top');
-const list = document.querySelectorAll('.header__nav__list');
+// menu open 
+let header = document.querySelector('.header__menu');
+let menu = document.querySelectorAll('.header__nav__top');
+let list = document.querySelectorAll('.header__nav__list');
 
-if (clickPlace) {
-    clickPlace.forEach((element, i) => {
-    element.addEventListener('click', function() { 
-        list[i].classList.toggle('header__basket_list_on');
+if(menu) {
+    menu.forEach((element, i) => {
+        element.addEventListener('mouseenter', function() {
+            list[i].classList.toggle('open');
     });
 })};
 
+if(list) {
+    list.forEach((element, i) => {
+        element.addEventListener('mouseleave', function() {
+            list[i].classList.toggle('open');
+    });
+})};
 
-$(document).ready(function() {
+// order open by click
+
+let basket = document.querySelector('.header__basket');
+let bTop = basket.querySelector('.header__basket_top');
+
+bTop.onclick = function() {
+    basket.classList.toggle('open');
+};
+
+
+
+// jQuerry 
+
+//$(document).ready(function() { 
 
 // открытие меню и корзины по клику
     // $('.header__nav__top').on('click', function(){
@@ -28,14 +48,14 @@ $(document).ready(function() {
     //     });
  
 // открытие корзины при наведении 
-    $('#order-arrow').on('mouseenter', function(){
-        $('.header__basket_list').fadeToggle(300)
-    });
-    $('.header__basket_list').on('mouseleave', function(){
-        $(this).fadeToggle(300)
-    });
+    // $('#order-arrow').on('mouseenter', function(){
+    //     $('.header__basket_list').fadeToggle(300)
+    // });
+    // $('.header__basket_list').on('mouseleave', function(){
+    //     $(this).fadeToggle(300)
+    // });
 
-//увеличение элементов в фокусе    
+//увеличение элементов в фокусе > 1024px   
     // $('.promo__item').on('mouseenter', function() {
     //     $(this).animate({'width': '21vw'}, 1);
     //     $(this).animate({'height': '21vw'}, 1);
@@ -46,22 +66,4 @@ $(document).ready(function() {
     //     $(this).animate({'height': '20vw'}, 1);
     // })
 
-    
-
-// SEREJIN PLAN
-// const body = document.querySelector ('body')
-// const clickPlace = document.querySelector ('.header__nav__top')
-// const list = document.querySelector ('.header__nav__list')
-
-// if (body && clickPlace) {
-//     body.addEventListener('click', function() {
-//     if(clickPlace.click){
-//         list = false;
-//         }
-//     })
-//     list.addEventListener('click', function(e) {
-//         e.stopPropagation()
-//     })
-// }
-
-});
+//});
